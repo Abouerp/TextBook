8f11194d586081f88ae4d77db8c4c0dc8ce780cb
Abouerp <1057240821@qq.com>
2019-12-01 11:28:21 +0800

    add testclass

---
+++ src/test/java/com/it666/textbook/dao/TextBookDaoTest.java
@@ -0,0 +1,29 @@
+package com.it666.textbook.dao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * @author Abouerp
+ */
+class TextBookDaoTest {
+
+    @Autowired
+    TextBookDao textBookDao;
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void edit() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
---
+++ src/test/java/com/it666/textbook/dao/UserDaoTest.java
@@ -0,0 +1,31 @@
+package com.it666.textbook.dao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Abouerp
+ */
+class UserDaoTest {
+
+    @Autowired
+    UserDao userDao;
+
+    @Test
+    void findUserByUsername() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void edit() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
