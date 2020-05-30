8abb58cf4e7e2d32dd1799baa39d3885a880928b
Abouerp <1057240821@qq.com>
2020-05-30 16:04:36 +0800

    fix deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,8 +3,8 @@
 git fetch && \
 git reset --hard origin/master
 
-chmod +x ./gradlew && \
-./gradlew clean yarnBuild jibDockerBuild
+chmod +x ./mvnw && \
+./mvnw clean jibDockerBuild
 docker-compose up -d
 docker images | awk '$1 == "<none>" || $2 == "<none>" {print $3}' | xargs docker rmi
 
