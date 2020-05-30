d4fe399045b6de9fb5ef918bb3e4f0dac87f2679
Abouerp <1057240821@qq.com>
2020-05-31 01:21:56 +0800

    fix

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,7 +4,7 @@ git fetch && \
 git reset --hard origin/master
 
 chmod +x ./mvnw && \
-./mvnw clean jibDockerBuild
+./mvn clean jibDockerBuild
 docker-compose up -d
 docker images | awk '$1 == "<none>" || $2 == "<none>" {print $3}' | xargs docker rmi
 
