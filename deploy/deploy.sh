d322039b02aa69ca6f95d7f8e17451c4ea65451f
Abouerp <1057240821@qq.com>
2020-05-31 17:03:06 +0800

    fix

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,6 +5,6 @@ git reset --hard origin/master
 
 chmod +x ./mvnw && \
 ./mvnw clean compile jib:dockerBuild
-docker images | awk '$1 == "<none>" || $2 == "<none>" {print $3}' | xargs docker rmi
+#docker images | awk '$1 == "<none>" || $2 == "<none>" {print $3}' | xargs docker rmi
 
 echo "deploy success"
