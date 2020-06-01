f3d1fcaa775d890fe5e25f48d776ac1ed5f1d26d
Abouerp <1057240821@qq.com>
2020-06-01 19:08:15 +0800

    compose

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,7 +4,9 @@ git fetch && \
 git reset --hard origin/master
 
 chmod +x ./mvnw && \
-./mvnw clean compile jib:dockerBuild
+./mvnw clean jib:dockerBuild
+docker-compose up -d
+docker images | awk '$1 == "<none>" || $2 == "<none>" {print $3}' | xargs docker rmi
 
 
 echo "deploy success"
